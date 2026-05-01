d0a157b55e0deb236a13f4387fb533f1694331bb
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-05-01 04:00:28 -0300

    chore: adicionar funções utilitárias para verificação de tipos e valores

--- src/utils.ts
+++ src/utils.ts
@@ -1,35 +1,35 @@
 export function isNil(value: unknown): value is null | undefined {
-  return value === null || value === undefined
+  return value === null || value === undefined;
 }
 
 export function isString(value: unknown): value is string {
-  return typeof value === 'string'
+  return typeof value === 'string';
 }
 
 export function isArray<T = unknown>(value: unknown): value is T[] {
-  return Array.isArray(value)
+  return Array.isArray(value);
 }
 
 export function isObject(value: unknown): value is object {
-  return typeof value === 'object' && !isNil(value) && !isArray(value)
+  return typeof value === 'object' && !isNil(value) && !isArray(value);
 }
 
 export function isEmpty(value: unknown): boolean {
   if (isNil(value)) {
-    return true
+    return true;
   }
 
   if (isString(value)) {
-    return value.trim().length === 0
+    return value.trim().length === 0;
   }
 
   if (isArray(value)) {
-    return value.length === 0
+    return value.length === 0;
   }
 
   if (isObject(value)) {
-    return Object.keys(value).length === 0
+    return Object.keys(value).length === 0;
   }
 
-  return false
+  return false;
 }
