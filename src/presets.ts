8cf2c502b61a2527a814507bc7f37025526cc74c
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-05-09 21:22:29 -0300

    chore: atualizar os tipos de entrada nas funções de presets para usar number em vez de MoneyInput

--- src/presets.ts
+++ src/presets.ts
@@ -1,18 +1,18 @@
-import type { MoneyContract, MoneyInput } from './types';
+import type { MoneyContract } from './types';
 
 import { from } from '@/api/creation';
 
-export const au = (value: MoneyInput): MoneyContract => from(value, 'AU');
-export const br = (value: MoneyInput): MoneyContract => from(value, 'BR');
-export const ca = (value: MoneyInput): MoneyContract => from(value, 'CA');
-export const ch = (value: MoneyInput): MoneyContract => from(value, 'CH');
-export const cn = (value: MoneyInput): MoneyContract => from(value, 'CN');
-export const de = (value: MoneyInput): MoneyContract => from(value, 'DE');
-export const fr = (value: MoneyInput): MoneyContract => from(value, 'FR');
-export const gb = (value: MoneyInput): MoneyContract => from(value, 'GB');
-export const ind = (value: MoneyInput): MoneyContract => from(value, 'IN');
-export const jp = (value: MoneyInput): MoneyContract => from(value, 'JP');
-export const mx = (value: MoneyInput): MoneyContract => from(value, 'MX');
-export const sg = (value: MoneyInput): MoneyContract => from(value, 'SG');
-export const pt = (value: MoneyInput): MoneyContract => from(value, 'PT');
-export const us = (value: MoneyInput): MoneyContract => from(value, 'US');
+export const au = (value: number): MoneyContract => from(value, 'AU');
+export const br = (value: number): MoneyContract => from(value, 'BR');
+export const ca = (value: number): MoneyContract => from(value, 'CA');
+export const ch = (value: number): MoneyContract => from(value, 'CH');
+export const cn = (value: number): MoneyContract => from(value, 'CN');
+export const de = (value: number): MoneyContract => from(value, 'DE');
+export const fr = (value: number): MoneyContract => from(value, 'FR');
+export const gb = (value: number): MoneyContract => from(value, 'GB');
+export const ind = (value: number): MoneyContract => from(value, 'IN');
+export const jp = (value: number): MoneyContract => from(value, 'JP');
+export const mx = (value: number): MoneyContract => from(value, 'MX');
+export const sg = (value: number): MoneyContract => from(value, 'SG');
+export const pt = (value: number): MoneyContract => from(value, 'PT');
+export const us = (value: number): MoneyContract => from(value, 'US');
