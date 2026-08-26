e3b2bfda2dbcac290d3253793fd70e8746a0ed9b
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-08-26 06:13:11 -0300

    refactor: corrigir a ordem das funções no preset de criação de moeda

--- src/presets.ts
+++ src/presets.ts
@@ -6,8 +6,8 @@ function createPreset(country: CountryCode) {
   return {
     from: (value: number) => from(value, country),
     parse: (value: string) => parse(value, country),
-    zero: () => zero(country),
     fromMinorUnits: (value: number) => fromMinorUnits(value, country),
+    zero: () => zero(country),
   }
 }
 
